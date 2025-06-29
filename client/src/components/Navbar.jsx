1b601b31998a6e25630407cad0b8ac6ccdf132b4
nelson <chinwubanelson@gmail.com>
2025-06-29 09:00:14 +0100

    with real  navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,200 +1,132 @@
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdClose } from "react-icons/md";
-import { FaFacebookF } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { assets } from "../assets/assets";
+import { NavLink, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { FiHome } from "react-icons/fi";
+import { BsFillBoxSeamFill } from "react-icons/bs";
+import { AiFillMedicineBox } from "react-icons/ai";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
-const mobileStyle = "cursor-pointer text-white w-[100px]";
-const liStyles =
-  "hover:cursor-pointer  text-white  hover:border-b-2 border-orange-500  font-semibold  px-2 ";
-
+const listStyle =
+  "cursor-pointer px-2 flex flex-col justify-center item-center text-primary ";
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { token } = useContext(AppContext);
-
+  const { userData, backendUrl, setUserData, setIsLoggedIn } =
+    useContext(AppContext);
+  const navigate = useNavigate();
+  const sendVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(
+        backendUrl + "/api/auth/send-verify-otp"
+      );
+      if (data.success) {
+        navigate("/email-verification");
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const { data } = await axios.post(backendUrl + "/api/auth/logout");
+      data.success && setIsLoggedIn(false);
+      data.success && setUserData(false);
+      navigate("/");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
-    <header className=" fixed   h-[80px]  w-full  flex justify-center items-center bg-green-800 z-40">
+    <header className=" fixed w-full h-[80px]  flex items-center  bg-gray-50 z-40">
       <div className="container">
-        <div className="flex items-center hover:cursor-pointer">
+        <div className="flex justify-between items-center">
           {/* logo */}
-          <div className="flex-1 flex items-center gap-1 ">
-            <Link
-              onClick={() => scrollTo(0, 0)}
-              to={"/"}
-              className="flex  items-center justify-center gap-2"
+          <div className="">
+            <span
+              onClick={() => navigate("/")}
+              className="cursor-pointer text-blue-600 font-semibold"
             >
-              <span className=" p-[2px] bg-gradient-to-l  from-green-400 to-orange-600 rounded-full">
-                <img
-                  src="/logo.webp"
-                  loading="lazy"
-                  alt=""
-                  className="w-[50px] h-[50px] rounded-full "
-                />
-              </span>
-              <span className="text-xl font-bold text-orange-500 hidden md:block   ">
-                Cols-nutraceutical
-              </span>
-            </Link>
-          </div>
-
-          {/* nav links */}
-          <nav className=" flex justify-between items-center gap-3 flex-[2] relative">
-            <div className="flex-[3] hidden md:block ">
-              <ul className="flex justify-center items-center gap-5  font-semibold">
-                <li className={liStyles}>
-                  <Link onClick={() => scrollTo(0, 0)} to={"/"}>
-                    Home
-                  </Link>
-                </li>
-                <li className={liStyles}>
-                  <Link onClick={() => scrollTo(0, 0)} to={"/about"}>
-                    About
-                  </Link>
-                </li>
-                <li className={liStyles}>
-                  <Link onClick={() => scrollTo(0, 0)} to={"contact"}>
-                    Contact
-                  </Link>
-                </li>
-
-                <li className={liStyles}>
-                  <Link onClick={() => scrollTo(0, 0)} to={"/products"}>
-                    Products
-                  </Link>
-                </li>
-                {token && (
-                  <li className={liStyles}>
-                    <Link onClick={() => scrollTo(0, 0)} to={"/post"}>
-                      Post
-                    </Link>
-                  </li>
-                )}
-
-                {!token && (
-                  <li className={liStyles}>
-                    <Link onClick={() => scrollTo(0, 0)} to="/login">
-                      Admin
-                    </Link>
-                  </li>
-                )}
+              MedHunt
+            </span>
+          </div>{" "}
+          <div className="hidden md:flex justify-center items-center ">
+            <nav className="">
+              <ul className="flex justify-between items-center gap-3">
+                <NavLink to={"/"} className={listStyle}>
+                  <span className="flex items-center justify-center">
+                    <FiHome size={20} />
+                  </span>
+                  <span className="text-sm">Home</span>
+                </NavLink>
+                <NavLink to={"/jobs"} className={listStyle}>
+                  <span className="flex items-center justify-center">
+                    <BsFillBoxSeamFill size={20} />
+                  </span>
+                  <span className="text-sm">Jobs</span>
+                </NavLink>
+                <NavLink to={"/post-jobs"} className={listStyle}>
+                  <span className="flex items-center justify-center">
+                    <AiFillMedicineBox size={20} />
+                  </span>
+                  <span className="text-sm">Post a job</span>
+                </NavLink>
+                <NavLink to={"/notifications"} className={listStyle}>
+                  <span className="flex items-center justify-center">
+                    <IoMdNotificationsOutline size={25} />
+                  </span>
+                  <span className="text-sm">Notifications</span>
+                </NavLink>
               </ul>
-            </div>
-            <div className="flex-1 flex  items-center gap-4 justify-end ">
-              <div
-                onClick={() => setIsOpen((prev) => !prev)}
-                className="md:hidden z-40 cursor-pointer"
-              >
-                {isOpen ? (
-                  <MdClose color="white" size={30} />
-                ) : (
-                  <GiHamburgerMenu size={30} color="white" />
-                )}
+            </nav>
+          </div>
+          <div className="">
+            {userData ? (
+              <div className="h-5 flex justify-center items-center cursor-pointer  text-blue-700 rounded-md  relative group">
+                <span className="text-base  px-3 py-1 border-2 border-blue-500 rounded-md">
+                  {/* {userData.name} */}
+                  My profile
+                </span>{" "}
+                <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
+                  <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+                    {!userData.isAccountVerified && (
+                      <li
+                        onClick={sendVerificationOtp}
+                        className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
+                      >
+                        Verify Email
+                      </li>
+                    )}
+                    <li
+                      onClick={logout}
+                      className="py-1 px-2 hover:bg-gray-200 text-red-600 cursor-pointer"
+                    >
+                      Logout
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          </nav>
-          {/* Mobile nav */}
-          <nav
-            className={`${
-              isOpen ? "right-0" : "right-[-100%]"
-            } md:hidden absolute  top-0 h-screen  w-[50%] bg-orange-500   transition-all duration-700 ease-out z-30`}
-          >
-            <ul className="text-black mt-20 pt-10 flex flex-col gap-6 items-center font-bold  ">
-              <li
-                onClick={() => setIsOpen((prev) => !prev)}
-                className={mobileStyle}
-              >
-                <Link onClick={() => scrollTo(0, 0)} to={"/"} className="">
-                  Home
-                </Link>
-              </li>
-              <li
-                onClick={() => setIsOpen((prev) => !prev)}
-                className={mobileStyle}
-              >
-                <Link onClick={() => scrollTo(0, 0)} to={"/about"} className="">
-                  About
-                </Link>
-              </li>
-
-              <li
-                onClick={() => setIsOpen((prev) => !prev)}
-                className={mobileStyle}
-              >
-                <Link
-                  onClick={() => scrollTo(0, 0)}
-                  to={"/contact"}
-                  className=""
+            ) : (
+              <div className="flex items-center ">
+                <button
+                  onClick={() => navigate("/register")}
+                  className="flex items-center gap-2 border border-blue-500  px-4 py-2 text-gray-800 hover:bg-gray-100 rounded-md"
                 >
-                  Contact
-                </Link>
-              </li>
-              <li
-                onClick={() => setIsOpen((prev) => !prev)}
-                className={mobileStyle}
-              >
-                <Link
-                  onClick={() => scrollTo(0, 0)}
-                  to={"/products"}
-                  className=""
-                >
-                  Products
-                </Link>
-              </li>
-              {token && (
-                <li
-                  onClick={() => setIsOpen((prev) => !prev)}
-                  className={mobileStyle}
-                >
-                  <Link
-                    onClick={() => scrollTo(0, 0)}
-                    to={"/post"}
-                    className=""
-                  >
-                    Post
-                  </Link>
-                </li>
-              )}
-              <li
-                onClick={() => setIsOpen((prev) => !prev)}
-                className={mobileStyle}
-              >
-                {!token && (
-                  <Link onClick={() => scrollTo(0, 0)} to="/login">
-                    Admin
-                  </Link>
-                )}
-              </li>
-            </ul>
-            {/* social links */}
-            <div className="flex items-center justify-center gap-5 pt-10 ">
-              <div className=" bg-blue-700 p-2 rounded-full">
-                <a
-                  href="https://www.facebook.com/share/1BsrNLBAEd/"
-                  target="_blank"
-                  className=""
-                >
-                  <span className="">
-                    <FaFacebookF size={15} color="white" />
-                  </span>
-                </a>
+                  Login{" "}
+                  <img
+                    src={assets.arrow_icon}
+                    alt="arrow_icon"
+                    className="w-2"
+                  />
+                </button>
               </div>
-
-              <div className="bg-white p-2 rounded-full">
-                <a
-                  href="https://www.instagram.com/kaceefresh"
-                  target="_blank"
-                  className=""
-                >
-                  <span className="">
-                    <FaInstagram size={15} color="red" />
-                  </span>
-                </a>
-              </div>
-            </div>
-          </nav>
+            )}
+          </div>
         </div>
       </div>
     </header>
