c1d07df3232346121b946d2bd98a35cdfa3c91d7
nelson <chinwubanelson@gmail.com>
2025-06-29 01:20:47 +0100

    centered home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
-    <main className="pt-20">
+    <main className="pt-20 w-full h-full flex flex-col items-center">
       <Hero />
       <LatestJobPost />
       <WhyChooseUs />
